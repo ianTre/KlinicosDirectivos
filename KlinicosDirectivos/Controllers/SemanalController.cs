1d8149678db5b4abcc41ca883a5eb86f025c3fd6
Unknown <ianntrejo96@gmail.com>
2019-11-28 15:30:54 -0300

    First Code
    First code with .Sln

---
+++ KlinicosDirectivos/Controllers/SemanalController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KlinicosDirectivos.Controllers
+{
+    public class SemanalController : Controller
+    {
+        // GET: Semanal
+        public ActionResult Index()
+        {
+            KlinicosEntities entidades = new KlinicosEntities();
+            List<Sectores> listaSectores = entidades.Sectores.ToList();
+            List<SelectListItem> sectores = new List<SelectListItem>();
+            foreach (var sector in listaSectores)
+            {
+                sectores.Add(new SelectListItem() { Text = sector.nombre, Value = sector.id.ToString() });
+            }
+
+
+
+
+            ViewBag.Sectores = sectores;
+            return View("Inicio");
+        }
+
+        public ActionResult SemanalEstablecimiento(int idEstablecimiento)
+        {
+            //KlinicosEntities entities = new KlinicosEntities();
+            //entities.Atenciones.Join(
+            //    entities.)
+
+            return View("Semanal");
+        }
+
+    }
+}
