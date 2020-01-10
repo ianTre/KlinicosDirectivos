73e7e302d6908e6d833f541dd523f05c6f771e56
Unknown <ianntrejo96@gmail.com>
2020-01-10 11:08:44 -0300

    Stash 10-1

---
+++ KlinicosDirectivos/Controllers/ProfesionalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KlinicosDirectivos.Controllers
+{
+    public class ProfesionalController : Controller
+    {
+        // GET: Profesional
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
