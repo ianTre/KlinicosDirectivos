b1a3a70a09173c0da81a7fece80479e15b66fada
Gonza <gonzajordan23@hotmail.com>
2020-03-25 11:57:02 -0300

    FrontEnd + Funciones Final

--- KlinicosDirectivos/Controllers/HomeController.cs
+++ KlinicosDirectivos/Controllers/HomeController.cs
@@ -11,6 +11,8 @@ namespace KlinicosDirectivos.Controllers
 {
     public class HomeController : Controller
     {
+      
+
         public ActionResult Index()
         {
             Klinicos_BEntities entidades = new Klinicos_BEntities();
@@ -25,8 +27,6 @@ namespace KlinicosDirectivos.Controllers
             ViewBag.Establecimientos = establecimientos;
             return View("Index");
         }
-
-        //Crear metodo llamado "Establecimiento" donde reciba idEstablecimiento
 
         public ActionResult establecimiento(int id)
         {
@@ -45,22 +45,85 @@ namespace KlinicosDirectivos.Controllers
 
 
 
-            return RedirectToAction("Index", "Semanal", new {lugar = nombreEsta.nombre});
+            return RedirectToAction("Index", "Semanal", new { lugar = nombreEsta.nombre });
         }
 
+        ////Crear metodo que reciba id establecimiento y devuelva IP
 
+        ////Lista de IP: 
+        ////Cemefir: 192.168.71.10
+        ////Eizaguirre: 172.48.1.10
+        ////Germani: 172.28.1.220
+        ////Giovinazzo: 172.26.1.10
+        ////Niños: 172.29.1.100
+        ////Policlinico: 192.168.34.10
+        ////Rebasa: 172.46.1.10
+        ////Sakamoto: 172.45.1.10
+        ////Salud Mental: 192.168.70.10
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        //private double ip;
 
-            return View();
-        }
+        //public ActionResult ObtenerIp(int id)
+        //{
+        //    Klinicos_BEntities entities = new Klinicos_BEntities();
 
-        public ActionResult Contact()
-        {
-            return View();
-        }
+        //    var establecimientosTotales = entities.Establecimientos.ToList();
+        //    Establecimientos establecimientos = new Establecimientos();
 
+        //    foreach (var item in establecimientosTotales)
+        //    {
+        //        switch (id)
+        //        {
+        //            //Cemefir: 192.168.71.10
+        //            case 15:
+        //                ip = 192.168.71.10;
+        //                break;
+
+        //            //Rebasa: 172.46.1.10
+        //            case 10:
+        //                ip = 172.46.1.10;
+        //                break;
+
+        //            //Eizaguirre: 172.48.1.10
+        //            case 11:
+        //                ip = 172.48.1.10;
+        //                break;
+
+        //            //Germani: 172.28.1.220
+        //            case 17:
+        //                ip = 172.28.1.220;
+        //                break;
+
+        //            //Giovinazzo: 172.26.1.10
+        //            case 9:
+        //                ip = 172.26.1.10;
+        //                break;
+
+        //            //Niños: 172.29.1.100
+        //            case 3:
+        //                ip = 172.29.1.100;
+        //                break;
+
+        //            //Policlinico: 192.168.34.10
+        //            case 4:
+        //                ip = 192.168.34.10;
+        //                break;
+
+        //            //Sakamoto: 172.45.1.10
+        //            case 8:
+        //                ip = 172.45.1.10;
+        //                break;
+
+        //            //Salud Mental: 192.168.70.10
+        //            case 14:
+        //                ip = 192.168.70.10;
+        //                break;
+        //            default:
+        //                break;
+        //        }
+
+        //        return (ip);
+        //    }
+        //}
     }
 }
